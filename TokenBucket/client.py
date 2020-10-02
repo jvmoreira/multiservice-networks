0de94573d63e54ab5ceaa88f7f5b6d8d95f91561
João Vitor Moreira <themoreira.jvm@gmail.com>
2020-10-02 00:17:09 -0300

    Refactor Token Bucket client

--- TokenBucket/client.py
+++ TokenBucket/client.py
@@ -2,33 +2,39 @@ import time
 import socket
 import random
 
-MAX_REQUESTS = 10000
+LOG_ENABLED = 0
+LOG_TO_FILE_ENABLED = 1
+SLEEP_ENABLED = 0
+MAX_REQUESTS = 100
 TIME_BETWEEN_REQUESTS = .001
-RATE = 7
-RANDOM_SEED = 1601327277
+RATE = 3
+RANDOM_SEED = 1601327277 # int( time.time() )
 random.seed(RANDOM_SEED)
 
-Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def printLog(requestSucceed, contentSentSize, contentReceived, originAddress):
+    if not LOG_ENABLED: return
 
-currentTime = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
-outputFile = open('log-' + currentTime + '.txt', 'w')
+    color = '1;32m' if requestSucceed else '1;31m'
+    padding = ' ' * ((15-contentSentSize) if requestSucceed else 7)
+    print('\x1b[{}Sent = {:02d} bytes | Received = [ {} ]{} | From = {}\x1b[0m'.format(color, contentSentSize, contentReceived, padding, originAddress))
+
 outputBuffer = ''
 
+Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 Socket.bind(('192.168.122.1', 8005))
-
-serverAddress = ('192.168.122.40', 8005)
-
-wordsToSend = []
+SERVER_ADDRESS = ('192.168.122.40', 8005)
 
 requestsSent = 0
 requestsSucceed = 0
 
-Socket.sendto('_reset _rate={}'.format(RATE), serverAddress)
+Socket.sendto('_reset _rate={}'.format(RATE), SERVER_ADDRESS)
 
 startTimestap = time.time()
 while(requestsSent < MAX_REQUESTS):
-    contentToSend = 'a' * random.randint(1,15)
-    Socket.sendto(contentToSend, serverAddress)
+    contentSize = random.randint(1,15)
+    contentToSend = 'a' * contentSize
+
+    Socket.sendto(contentToSend, SERVER_ADDRESS)
     contentTimestamp = int( 1e+3 * (time.time() - startTimestap) )
 
     [contentReceived, originAddress] = Socket.recvfrom(65000)
@@ -36,21 +42,23 @@ while(requestsSent < MAX_REQUESTS):
     requestSucceed = 'OK' in contentReceived
     if requestSucceed:
         requestsSucceed = requestsSucceed + 1
-        color = '1;32m'
-    else:
-        color = '1;31m'
 
-    padding = ' ' * ((15-len(contentToSend)) if requestSucceed else 7)
-    #print('\x1b[{}Sent = {:02d} bytes | Received = [ {} ]{} | From = {}\x1b[0m'.format(color, len(contentToSend), contentReceived, padding, originAddress))
-    packageStatus = 'OK' if requestSucceed else 'DROPPED'
-    outputBuffer += ('[{:d}] Sent = {:02d} bytes | Result = {}\n'.format(contentTimestamp, len(contentToSend), packageStatus))
+    outputBuffer += ('[{:d}] Sent = {:02d} bytes | Result = {}\n'.format(contentTimestamp, contentSize, 'OK' if requestSucceed else 'DROPPED'))
+    printLog(requestSucceed, contentSize, contentReceived, originAddress)
 
     requestsSent = requestsSent + 1
 
+    if SLEEP_ENABLED: time.sleep(TIME_BETWEEN_REQUESTS)
+
 requestsDropped = MAX_REQUESTS - requestsSucceed
-outputFile.write('Requests Succeed = {} | Requests Dropped = {} | Total Requests = {}\n\n'.format(requestsSucceed, requestsDropped, MAX_REQUESTS))
-outputFile.write('Bucket size = 50 | Rate = {}\n\n'.format(RATE))
-outputFile.write('Randomized with seed {}\n\n'.format(RANDOM_SEED))
-outputFile.write(outputBuffer)
+
+if LOG_TO_FILE_ENABLED:
+    currentDatetime = time.strftime("%Y%m%d-%H%M%S", time.localtime())
+    outputFile = open('log-rate-{}-{}.txt'.format(RATE, currentDatetime), 'w')
+
+    outputFile.write('Requests Succeed = {} | Requests Dropped = {} | Total Requests = {}\n\n'.format(requestsSucceed, requestsDropped, MAX_REQUESTS))
+    outputFile.write('Bucket size = 50 | Rate = {}\n\n'.format(RATE))
+    outputFile.write('Randomized with seed {}\n\n'.format(RANDOM_SEED))
+    outputFile.write(outputBuffer)
 
 Socket.close()
