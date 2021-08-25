aab4dc1be36c3e07e1dd619e0154cb986bd6c8ab
fequiles <feliperibeiro173@gmail.com>
2021-08-24 21:18:21 -0300

    Atualizacao para SocketRaw, implementacao do one rate policer e one rate marker com color aware de one rate

--- policing/one-rate-three-color.py
+++ policing/one-rate-three-color.py
@@ -1,6 +1,35 @@
 import threading #thread module imported
 import time #time module
 import socket
+import lib.packetProcessing as pp
+
+def colorAware(message, color):
+    global ca_bucketF_size, ca_bucketS_size, ca_dropped
+    [contentReceived, originAddress] = message
+    packet_size = pp.ipPacketSize(contentReceived)
+    if color == "Green":
+        if ca_bucketF_size < packet_size:
+            ca_dropped.append(message)
+            if debug: print("ColorAware: Red")
+        else:
+            ca_bucketF_size =- packet_size
+            if debug: print("ColorAware: Green")
+    elif color == "Yellow":
+        if ca_bucketS_size < packet_size:
+            ca_dropped.append(message)
+            if debug: print("ColorAware: Red")
+        else:
+            ca_bucketS_size =- packet_size
+            if debug: print("ColorAware: Yellow")
+    else:
+        ca_dropped.append(message)
+        if debug: print("ColorAware: Red")
+
+
+def colorAwareBucketRate():
+    global ca_bucketF_size, ca_bucketS_size, ca_rate, ca_bucketF_max_size, ca_bucketS_max_size
+    ca_bucketF_size += ca_rate if ca_bucketF_size + ca_rate <= ca_bucketF_max_size else ca_bucketF_max_size
+    ca_bucketS_size += ca_rate if ca_bucketS_size + ca_rate <= ca_bucketS_max_size else ca_bucketS_max_size
 
 def thread_Time(thread_name, interval):
     global semaphore, rate, bucketF_size, bucketF_max_size, bucketS_size, bucketS_max_size
@@ -8,44 +37,65 @@ def thread_Time(thread_name, interval):
         semaphore.acquire()
         bucketF_size += rate if bucketF_size + rate <= bucketF_max_size else bucketF_max_size
         bucketS_size += rate if bucketS_size + rate <= bucketS_max_size else bucketS_max_size
+        if color_aware: colorAwareBucketRate()
         semaphore.release()
         time.sleep(interval)
-
+        
 def thread_TokenBucket():
 #Funcao que quando chega pacote e nao tem pacotes na fila entao envia ou adiciona na fila
-    global Socket, bucketF_size, semaphore, bucketF_max_size, bucketS_size, bucketS_max_size, dropped
+    global Socket, bucketF_size, semaphore, bucketS_size, dropped, debug
     while 1:
         message = Socket.recvfrom(65000)
         [contentReceived, originAddress] = message
-        packet_size = int(contentReceived.decode())
+        packet_size = pp.ipPacketSize(contentReceived)
         semaphore.acquire()
         if bucketF_size < packet_size:
             if bucketS_size < packet_size:
-                dropped.append(message)
-                print("Mensagem dropada(Red): " + str(message))
+                if debug: print("Mensagem marcada: Red")
+                if color_aware:
+                    colorAware(message, "Red")
+                else:
+                    dropped.append(message)
             else:
-                print("Transmitindo pacote(Yellow): " + str(message))
+                if debug: print("Mensagem marcada: Yellow")
+                if color_aware: colorAware(message, "Yellow")
                 bucketS_size -= packet_size
         else:
-            print("Transmitindo pacote(Green):" + str(message))
+            if debug: print("Mensagem marcada: Green")
+            if color_aware: colorAware(message, "Green")
             bucketF_size -= packet_size
         semaphore.release()
 
 dropped = []
 
-# rate = 2
-# bucketF_size = 20
-# bucketF_max_size = 30
-# bucketS_size = 20
-# bucketS_max_size = 30
-# interval = 1.0
-# host_address = '127.0.0.1'
-# target_address = 0
+#rate = 100
+#bucketF_size = 1000
+#bucketF_max_size = 2000
+#bucketS_size = 2000
+#bucketS_max_size = 4000
+#interval = 1
+#interface = 'wlp0s20f3'
+#debug = 1
+#color_aware = 1
+#ca_bucketF_size = 500
+#ca_bucketF_max_size = 1000
+#ca_bucketS_size = 800
+#ca_bucketS_max_size = 1200
+#ca_rate = 100
 
 #__PARAMETERS__
 
-Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-Socket.bind((host_address, 8005))
+if color_aware: 
+    ca_dropped = [] 
+else: 
+    ca_bucketF_size = 0
+    ca_bucketF_max_size = 0
+    ca_bucketS_size = 0
+    ca_bucketS_max_size = 0
+    ca_rate = 0
+    ca_dropped = []
+
+Socket = pp.socketStart(interface)
 
 semaphore = threading.Semaphore(1)
 timer = threading.Thread(target=thread_Time, args=('timer', interval))
@@ -53,7 +103,3 @@ token_bucket = threading.Thread(target=thread_TokenBucket, args=())
 
 timer.start()
 token_bucket.start()
-
-for i in range(1, 11):
-    message = str(i).encode()
-    Socket.sendto(message, (host_address, 8005))
