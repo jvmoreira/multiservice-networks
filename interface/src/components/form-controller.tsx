838343061d0096ce636ce6bbeb44062b0a060479
João Vitor Moreira <themoreira.jvm@gmail.com>
2022-04-14 02:33:51 -0300

    Display NFV-TE Values formatted as JSON

--- interface/src/components/form-controller.tsx
+++ interface/src/components/form-controller.tsx
@@ -1,13 +1,40 @@
-import React, { ReactElement } from 'react';
-import { NfvTeValuesContextProvider, useNfvTeValuesState } from '@/commons/nfv-te-values';
+import React, { ReactElement, useMemo } from 'react';
+import { NfvTeValues, NfvTeValuesContextProvider, useNfvTeValuesState } from '@/commons/nfv-te-values';
 import { Form } from '@/components/form';
+import { Display } from '@/components/display';
 
 export function FormController(): ReactElement {
   const [nfvTeValues, setNfvTeValues] = useNfvTeValuesState();
+  const formattedNfvTeValues = useMemo(() => formatNfvTeValues(nfvTeValues), [nfvTeValues]);
 
   return (
     <NfvTeValuesContextProvider value={{ nfvTeValues, setNfvTeValues }}>
-      <Form />
+      <main>
+        <Form />
+        <Display value={formattedNfvTeValues} />
+      </main>
     </NfvTeValuesContextProvider>
   );
+}
+
+function formatNfvTeValues(nfvTeValues: NfvTeValues): string {
+  const functionParametersEntries = Object.entries(nfvTeValues.functionParameters);
+  const functionParameters = functionParametersEntries.reduce((acc, [paramName, paramValue]) => {
+    if (!paramValue) {
+      return acc;
+    }
+
+    return `${acc}\n  "${paramName}": ${paramValue},`;
+  }, '');
+
+  return `{
+  "category": "${nfvTeValues.category}",
+  "function": "${nfvTeValues.functionName}",
+
+${functionParameters}
+
+  "client-interface": "${nfvTeValues.clientInterface}",
+  "server-interface": "${nfvTeValues.serverInterface}",
+  "debug": ${nfvTeValues.debug},
+}\n`;
 }
